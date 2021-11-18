c7623b69e28069555d9ad3c2735850cc6d85cf47
Sergjio97 <sergiocizmja@gmail.com>
2021-11-18 09:14:25 +0100

    easy bgc test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 alert("ciao");
 
 
+const easy = document.querySelector(".box_easy");
+
 document.querySelector(".box_easy").addEventListener("click", function() {
     alert("funziona");
+    easy.classList.add("blue");
 });
+
+
